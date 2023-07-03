210f096840edcd064dd986e941ea34bab585db56
Palpitate <xus1758011080@outlook.com>
2023-07-03 17:19:17 +0800

    修改mock

--- mock/controller/user.js
+++ mock/controller/user.js
@@ -82,14 +82,4 @@ module.exports = [
       }
     },
   },
-  {
-    url: '/logout',
-    type: 'post',
-    response() {
-      return {
-        code: 200,
-        msg: 'success',
-      }
-    },
-  },
 ]
